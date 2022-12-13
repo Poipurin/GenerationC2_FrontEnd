db0bb82ec2674f07f6a6548ae344b28f9c2abe5f
Poipurin <b.pinorojo@gmail.com>
2022-12-13 10:42:52 -0300

    borrar importaciones

--- Html/react/my-app/src/App.js
+++ Html/react/my-app/src/App.js
@@ -2,8 +2,6 @@
 import Header from './components/header';
 import Aside from './components/aside';
 import Container from './components/container';
-import Subcontainer from './components/subcontainer';
-import Advertisement from './components/advertisement';
 import Wrapper from './components/wrapper';
 import './App.css';
 
