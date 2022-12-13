6e168cc801f08c7f436eff179ec8505347e5bbcd
Poipurin <b.pinorojo@gmail.com>
2022-12-13 10:13:11 -0300

    React

--- Html/react/my-app/src/App.js
+++ Html/react/my-app/src/App.js
@@ -1,10 +1,12 @@
 
+import Header from './components/header';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
     <h1>JSX Javascript syntax extension; html javascript</h1>
+    <Header />
     </div>
   );
 }
---
+++ Html/react/my-app/src/components/aside.js
@@ -0,0 +1,7 @@
+import React,{Component} from 'react';
+
+class Aside extends Component {
+
+}
+
+export default Aside;
